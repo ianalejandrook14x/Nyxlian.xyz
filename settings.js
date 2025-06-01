d99116e6570706cf621a1259ae48624ceee98fae
Ian.xyz <taiwam34@gmail.com>
2025-06-01 18:57:44 -0300

    _

--- settings.js
+++ settings.js
@@ -12,7 +12,6 @@ global.confirmCode = ''
 
 global.owner = [
    ['5493876639332', 'Ian', true],
-   ['526561105057', 'AxelOfc', true],
 ]
 
 global.mods = []
@@ -23,20 +22,20 @@ global.libreria = 'Baileys'
 global.baileys = 'V 6.7.8'
 global.vs = '1.0.1'
 global.languaje = 'Español'
-global.nameqr = 'Frieren-AI'
+global.nameqr = 'Nιɳσ'
 global.sessions = 'BotSession'
 global.jadi = 'JadiBot'
 
-global.banner = 'https://qu.ax/zNqAj.jpg'
-global.botname = 'Frieren-AI'
-global.author = 'Dev-AxelOfc'
-global.dev = 'AxelOfc'
+global.banner = 'https://qu.ax/spLlO.jpg'
+global.botname = 'Nιɳσ'
+global.author = 'Nყxʅαɳ.xყȥ'
+global.dev = 'Nყxʅαɳ.xყȥ'
 global.currency = 'Yenes'
 global.botStatus = true;
 global.welcome = '*Bienvenido al grupo* 👋🏻\n> *Utilice .menu*\n*Para ver el menu del bot*'
 
-global.image = fs.readFileSync('./src/img/imagen.jpeg')
-global.avatar = fs.readFileSync('./src/img/avatar_contact.jpeg')
+global.image = fs.readFileSync('./image/imagen.jpeg')
+global.avatar = fs.readFileSync('./image/avatar_contact.jpeg')
 
 global.grupo = 'https://chat.whatsapp.com/KiJY7gZS17aLcP6qaGatFX'
 global.channel = 'https://whatsapp.com/channel/0029VajkZ6bIXnlwPZmbuH1u'
