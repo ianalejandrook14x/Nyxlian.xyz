d86589428d267ff28a881a9b2561a13938857b27
Ian.xyz <taiwam34@gmail.com>
2025-06-01 19:55:24 -0300

    _

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ if (collaborators.hasOwnProperty(key)) {
 activeCollaborators += collaborators[key] + ', '
 }}
 activeCollaborators = activeCollaborators.slice(0, -2);
-cfonts.say('Nyxlan.xyz', {
+cfonts.say('Socket', {
 align: 'center',           
 gradient: ['white', 'black'] 
 })
